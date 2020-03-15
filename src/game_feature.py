b0b16b70d8b8a9060060a695b507a84f51267132
Sharon Yaroshetsky <imso3k@gmail.com>
2020-03-15 17:47:25 +0200

    0.4.0 - refactored the whole file

--- src/game_feature.py
+++ src/game_feature.py
@@ -1,49 +1,29 @@
 import requests
 
 
-def get_info(url, params=None):
-    if params is None:
-        params = {}
-    response = requests.get(url=url, params=params)
+def get_info(url):
+    response = requests.get(url=url)
     return response.json()
 
 
 class GameFeatureClass:
 
     @staticmethod
-    def CheckID(stub_id):
-        params = {
-            'page': 1,
-            'per_page': 1,
-        }
-        return get_info('https://www.balldontlie.io/api/v1/games', params)['data'][0][stub_id]
+    def CheckID(param):
+        return get_info('https://www.balldontlie.io/api/v1/games')['data'][0][param]
 
     @staticmethod
-    def SeasonsInfo(stub_season):
-        params = {
-            'page': 1,
-            'per_page': 1,
-        }
-        url = 'https://www.balldontlie.io/api/v1/games' + f'{stub_season}'
-        return get_info(url, params=params)['data'][0]['date']
+    def SeasonsInfo(param):
+        return get_info('https://www.balldontlie.io/api/v1/games')['data'][0][param]
 
     @staticmethod
-    def PostSeasonInfo(stub_ps_bool):
-        params = {
-            'page': 1,
-            'per_page': 20,
-        }
-        url = 'https://www.balldontlie.io/api/v1/games' + f'{stub_ps_bool}'
-        info = get_info(url, params=params)['data']
-        for status in info:
-            if status['postseason'] is True:
-                return True
+    def PostSeasonInfo(param):
+        url = 'https://www.balldontlie.io/api/v1/games'
+        info = get_info(url)['data'][0]
+        if info[param] is True:
+            return True
         return False
 
     @staticmethod
-    def ScoreInfo(stub_team_type):
-        params = {
-            'page': 1,
-            'per_page': 1,
-        }
-        return get_info('https://www.balldontlie.io/api/v1/games', params)['data'][0][stub_team_type]
+    def ScoreInfo(param):
+        return get_info('https://www.balldontlie.io/api/v1/games')['data'][0][param]
