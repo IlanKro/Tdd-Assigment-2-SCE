03e6bc91e6dc189fdb2f3de12fb9ee1763fe00a6
Sharon Yaroshetsky <imso3k@gmail.com>
2020-03-15 14:59:22 +0200

    0.2.0 - added SeasonInfo tests (date & time)

--- test/game_feature_test.py
+++ test/game_feature_test.py
@@ -21,19 +21,41 @@ class GameFeatureTest(unittest.TestCase):
                                   'meta': {'total_pages': 48754, 'current_page': 1, 'next_page': 2, 'per_page': 1,
                                            'total_count': 48754}}
 
+        game.twentysixteen.season.return_value = {'data': [{'id': 32809, 'date': '2016-11-17T00:00:00.000Z',
+                                                            'home_team': {'id': 29, 'abbreviation': 'UTA',
+                                                                          'city': 'Utah', 'conference': 'West',
+                                                                          'division': 'Northwest',
+                                                                          'full_name': 'Utah Jazz', 'name': 'Jazz'},
+                                                            'home_team_score': 77, 'period': 4, 'postseason': False,
+                                                            'season': 2016, 'status': 'Final', 'time': ' ',
+                                                            'visitor_team': {'id': 5, 'abbreviation': 'CHI',
+                                                                             'city': 'Chicago', 'conference': 'East',
+                                                                             'division': 'Central',
+                                                                             'full_name': 'Chicago Bulls',
+                                                                             'name': 'Bulls'},
+                                                            'visitor_team_score': 85}],
+                                                  'meta': {'total_pages': 1309, 'current_page': 1, 'next_page': 2,
+                                                           'per_page': 1, 'total_count': 1309}}
+
         # assume
         stub1 = 'id'
+        stub2 = '?seasons[]=2016'
 
         # expected
         expected1 = 47179
         expected2 = int
+        expected3 = '2016-11-17'
+        expected4 = '00:00:00.000Z'
 
         # action
         result1 = GameFeatureClass.CheckID(stub1)
+        result2 = GameFeatureClass.SeasonsInfo(stub2).split('T')
 
         # assert
         self.assertEqual(result1, expected1)
         self.assertIsInstance(result1, expected2)
+        self.assertEqual(result2[0], expected3)
+        self.assertEqual(result2[1], expected4)
 
 
 if __name__ == '__main__':
