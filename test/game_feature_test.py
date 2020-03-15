f16885feace85d3de78eec03dcd01887fb341ba5
Sharon Yaroshetsky <imso3k@gmail.com>
2020-03-15 15:43:09 +0200

    0.3.5 - added HomeTeam and VisitorTeam Checks & refactored file

--- test/game_feature_test.py
+++ test/game_feature_test.py
@@ -5,8 +5,8 @@ from src.game_feature import GameFeatureClass
 
 class GameFeatureTest(unittest.TestCase):
     @patch('src.game_feature.GameFeatureClass')
-    def test_page(self, MockGame):
-        game = MockGame()
+    def test_page(self, mock_game):
+        game = mock_game()
 
         game.data.return_value = {'data': [{'id': 47179, 'date': '2019-01-30T00:00:00.000Z',
                                             'home_team': {'id': 2, 'abbreviation': 'BOS', 'city': 'Boston',
@@ -40,22 +40,36 @@ class GameFeatureTest(unittest.TestCase):
         # assume
         stub1 = 'id'
         stub2 = '?seasons[]=2016'
+        stub3 = '?postseason[]=False'
+        stub4 = 'home_team_score'
+        stub5 = 'visitor_team_score'
 
         # expected
-        expected1 = 47179
-        expected2 = int
-        expected3 = '2016-11-17'
-        expected4 = '00:00:00.000Z'
+        expected_id = 47179
+        expected_id_type = int
+        expected_game_date_2016 = '2016-11-17'
+        expected_game_time_2016 = '00:00:00.000Z'
+        expected_game_dt_type = list
+        expected_home_team_score = 126
+        expected_visitor_team_score = 94
 
         # action
         result1 = GameFeatureClass.CheckID(stub1)
         result2 = GameFeatureClass.SeasonsInfo(stub2).split('T')
+        result3 = GameFeatureClass.PostSeasonInfo(stub3)
+        result4 = GameFeatureClass.ScoreInfo(stub4)
+        result5 = GameFeatureClass.ScoreInfo(stub5)
 
         # assert
-        self.assertEqual(result1, expected1)
-        self.assertIsInstance(result1, expected2)
-        self.assertEqual(result2[0], expected3)
-        self.assertEqual(result2[1], expected4)
+        self.assertEqual(result1, expected_id)
+        self.assertIsInstance(result1, expected_id_type)
+        self.assertEqual(result2[0], expected_game_date_2016)
+        self.assertEqual(result2[1], expected_game_time_2016)
+        self.assertIsInstance(result2, expected_game_dt_type)
+        self.assertFalse(result3)
+        self.assertEqual(result4, expected_home_team_score)
+        self.assertEqual(result5, expected_visitor_team_score)
+        self.assertGreater(result4, result5)
 
 
 if __name__ == '__main__':
