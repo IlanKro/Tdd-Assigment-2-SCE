1551247f6a255bc9a22d314752c12f76a1c86565
guille kroter <Ilankroter@gmail.com>
2020-03-16 18:18:30 +0200

    0.5 the test work and the API works with the mock!

--- test/player_feature_test.py
+++ test/player_feature_test.py
@@ -4,31 +4,27 @@ from src.player_feature import PlayerFeatureClass, get_info
 
 
 class PlayerFeatureTest(unittest.TestCase):
-    @patch('src.player_feature.PlayerFeatureClass')
-    def test_page(self, MockPlayer):
+    @patch('src.player_feature.requests.get')
+    def test_page(self, mock_player):
         """
         Testing a few elements from the player feature
-        :param MockPlayer: An input from patch.
+        :param mock_player: An input from patch.
         """
-        player = {'data': [{'id': 237, 'first_name': 'LeBron', 'height_feet': 6, 'height_inches': 8,
-                            'last_name': 'James', 'position': 'F', 'team': {'id': 14, 'abbreviation': 'LAL',
-                                                                            'city': 'Los Angeles', 'conference': 'West',
-                                                                            'division': 'Pacific',
-                                                                            'full_name': 'Los Angeles Lakers',
-                                                                            'name': 'Lakers'},
-                            'weight_pounds': 250}],
-                  'meta': {'total_pages': 1, 'current_page': 1, 'next_page': None, 'per_page': 25, 'total_count': 1}}
+        player = {'id': 237, 'first_name': 'LeBron', 'height_feet': 6, 'height_inches': 8,
+                  'last_name': 'James', 'position': 'F', 'team': {'id': 14, 'abbreviation': 'LAL',
+                                                                  'city': 'Los Angeles', 'conference': 'West',
+                                                                  'division': 'Pacific',
+                                                                  'full_name': 'Los Angeles Lakers',
+                                                                  'name': 'Lakers'}, 'weight_pounds': 250}
 
-        MockPlayer.return_value = Mock(ok=True)
-        MockPlayer.return_value.json.return_value = player
-        player2 = PlayerFeatureClass('id', player['data'][0]['id'])
-        player3 = PlayerFeatureClass('name', player['data'][0]['first_name'])
-
+        mock_player.return_value = Mock(ok=True)
+        mock_player.return_value.json.return_value = player  # setting the API to only return this mock object.
+        player2 = PlayerFeatureClass('id', player['id'])
         # expected
-        expected_id = player['data'][0]['id']
+        expected_id = player['id']
         expected_id_type = list
-        expected_first_name = player['data'][0]['first_name']
-        expected_last_name = player['data'][0]['last_name']
+        expected_first_name = player['first_name']
+        expected_last_name = player['last_name']
         expected_name_dt_type = list
         expected_weight_in_kilo = 113.39
         expected_height_in_meters = 2.06
@@ -46,10 +42,9 @@ class PlayerFeatureTest(unittest.TestCase):
         self.assertEqual(result2[0][1], expected_last_name)  # testing if the last name is the same.
         self.assertIsInstance(result2, expected_name_dt_type)  # testing if the give name is a list of names.
         self.assertAlmostEqual(result3['LeBron James']['weight'], expected_weight_in_kilo,
-                               delta=0.1)  # testing if the convertion is right.
+                               delta=0.1)  # testing if the conversion is right.
         self.assertAlmostEqual(result4['LeBron James']['height'], expected_height_in_meters,
-                               delta=0.1)  # testing if the convertion is right.
-        self.assertEqual(player2.player, player3.player)
+                               delta=0.1)  # testing if the conversion is right.
 
     if __name__ == '__main__':
         unittest.main()
