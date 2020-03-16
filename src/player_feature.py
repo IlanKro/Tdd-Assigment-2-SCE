a0e5915547a7f6d3a4617053063c034b270d2502
guille kroter <Ilankroter@gmail.com>
2020-03-16 16:28:10 +0200

    0.3 refactored the last method to return a dictionary, removed manual tests and added documentation

--- src/player_feature.py
+++ src/player_feature.py
@@ -4,6 +4,12 @@ import requests
 
 
 def get_info(url, params=''):
+    """
+    get an object or a list of objects from an API.
+    :param url: A url who returns a json object.
+    :param params: search specific pages in the API
+    :return: an object or a list of objects from an API.
+    """
     if params is None:
         params = {}
     try:
@@ -12,10 +18,17 @@ def get_info(url, params=''):
         return
     return response
 
-
 class PlayerFeatureClass:
-
+"""
+Makes an object of players from the NBA API
+"""
     def __init__(self, search_type, search_param):
+        """
+        Gets a NBA player list according to a criteria from the api.
+        :param search_type: 'id': according to id will find one player, 'name' to search for a name or part of a first/last
+        name of players
+        :param search_param: the parameter of search for id- a number for name a string.
+        """
         if search_type == 'id':
             self.player = [get_info('https://www.balldontlie.io/api/v1/players/' + str(search_param))]
             if None in self.player:
@@ -33,36 +46,25 @@ class PlayerFeatureClass:
         return [i['id'] for i in self.player]
 
     def CheckName(self):
+        """
+        Returns all the names of the player list
+        :return: return the names as items in a list [first_name,last_name]
+        """
         return [[item['first_name'], item['last_name']] for item in self.player]
 
     def MetricUnits(self, param):
-        imperial_to_metric = {'weight_pounds': 0.45359237, 'height_feet': 0.3048}
-        imperial = [item[param] for item in self.player]
-        metric = []
-        for item in imperial:
-            if item is not None:
-                metric.append(round(imperial_to_metric[param] * item, 2))
+        """
+        Translate a criteria in the player statics into metric.
+        :param param: weight_pounds or height_feet or 'height_inches'
+        to translate into kg for the first one or meters for the later 2
+        :return: A dictionary of translated units with player names as key.
+        """
+        imperial_to_metric = {'weight_pounds': 0.45359237, 'height_feet': 0.3048, 'height_inches': 0.0254}
+        metric = {}
+        for item in self.player:
+            if item[param] is not None:
+                metric[item['first_name'] + ' ' + item['last_name']] = (
+                    round(imperial_to_metric[param] * item[param], 2))
             else:
-                metric.append(None)
+                metric[item['first_name'] + ' ' + item['last_name']] = None
         return metric
-
-    # get_info('https://www.balldontlie.io/api/v1/players/',{'page': 2})
-
-
-# print(requests.get(url='https://www.balldontlie.io/api/v1/players/1').json())
-# print(get_info('https://www.balldontlie.io/api/v1/players/?search=Lebron'))
-# hello = get_info('https://www.balldontlie.io/api/v1/players/237')
-# print(len(hello))
-# print(hello)
-# print(PlayerFeatureClass.MetricUnits(237, 'weight_pounds'))
-"""
-LeBron = PlayerFeatureClass('name', 'LeB')
-LeBron2 = PlayerFeatureClass('id', '237')
-print(LeBron.player)
-print(LeBron2.player)
-print(LeBron.player == LeBron2.player)
-print(LeBron.CheckID())
-print(LeBron.CheckName())
-print(LeBron.player)
-print(LeBron.MetricUnits('weight_pounds'))
-"""
--- test/player_feature_test.py
+++ test/player_feature_test.py
@@ -7,9 +7,8 @@ class PlayerFeatureTest(unittest.TestCase):
     @patch('src.player_feature.PlayerFeatureClass')
     def test_page(self, MockPlayer):
         """
-
-        :param MockPlayer:
-        :return:
+        Testing a few elements from the player feature
+        :param MockPlayer: An input from patch.
         """
         player = {'data': [{'id': 237, 'first_name': 'LeBron', 'height_feet': 6, 'height_inches': 8,
                             'last_name': 'James', 'position': 'F', 'team': {'id': 14, 'abbreviation': 'LAL',
@@ -23,9 +22,11 @@ class PlayerFeatureTest(unittest.TestCase):
         MockPlayer.return_value = Mock(ok=True)
         MockPlayer.return_value.json.return_value = player
         player2 = PlayerFeatureClass('id', 237)
+        player3 = PlayerFeatureClass('name', 'LeBron')
         # assume
         stub1 = 'weight_pounds'
         stub2 = 'height_feet'
+        stub3= 'height_inches'
 
         # expected
         expected_id = 237
@@ -35,21 +36,23 @@ class PlayerFeatureTest(unittest.TestCase):
         expected_name_dt_type = list
         expected_weight_in_kilo = 113.4
         expected_height_in_meters = 1.83
-
         # action
         result1 = player2.CheckID()
         result2 = player2.CheckName()
         result3 = player2.MetricUnits(stub1)
         result4 = player2.MetricUnits(stub2)
+        result5 = player2.MetricUnits(stub3)
 
         # assert
-        self.assertEqual(result1[0],expected_id)
-        self.assertIsInstance(result1,expected_id_type)
-        self.assertEqual(result2[0][0],expected_first_name)
-        self.assertEqual(result2[0][1],expected_last_name)
-        self.assertIsInstance(result2,expected_name_dt_type)
-        self.assertAlmostEqual(result3[0],expected_weight_in_kilo, places=3)
-        self.assertAlmostEqual(result4[0],expected_height_in_meters, places=3)
+        self.assertEqual(result1[0], expected_id)  # testing if the id is equal to the mock
+        self.assertIsInstance(result1, expected_id_type)  # testing if the id is a list (although being a single number)
+        self.assertEqual(result2[0][0], expected_first_name)  # testing if the name is the same.
+        self.assertEqual(result2[0][1], expected_last_name)  # testing if the last name is the same.
+        self.assertIsInstance(result2, expected_name_dt_type)  # testing if the give name is a list of names.
+        self.assertAlmostEqual(result3['LeBron James'], expected_weight_in_kilo, places=3)  # testing if the convertion is right.
+        self.assertAlmostEqual(result4['LeBron James'], expected_height_in_meters, places=3)  # testing if the convertion is right.
+        self.assertAlmostEqual(result5['LeBron James'], expected_height_in_meters,places=3)  # testing if the convertion is right.
+        self.assertEqual(player2.player, player3.player)
 
     if __name__ == '__main__':
         unittest.main()
